17c31ead8e873b0a250640191c1bdce87eed93fa
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-05-02 04:36:53 -0700

    experience

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -14,15 +14,15 @@ function About(props) {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="about image" />
+            <img src={ME} alt="fortune" />
           </div>
         </div>
         <div className="about__content">
-          <div className="about_cards">
+          <div className="about__cards">
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>1+ Years Wokring Experience</small>
+              <small>1+ Years Working Experience</small>
             </article>
             <article className="about__card">
               <FaUsers className="about__icon" />
@@ -39,7 +39,9 @@ function About(props) {
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error
             eaque recusandae voluptatibus maiores, magni voluptates quas hic nam
-            officia cumque.
+            officia cumque. Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Corporis accusamus ex natus mollitia nisi, eius ea praesentium
+            ad sit? Temporibus!
           </p>
           <a href="#contact" className="btn btn-primary">
             Lets Talk
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,8 +1,87 @@
 import React from "react";
 import "./experience.css";
+import { GoVerified } from "react-icons/go";
 
 const Experience = (props) => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <GoVerified />
+              <h4>HTML</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4>CSS</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4>JavaScript</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4>TailwindCss</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4>Git/Github</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4>React</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__others">
+          <h3>Soft skils</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <GoVerified />
+              <h4> Ability to work as a team</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4> Excellent Problem solving skills</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4> Attention to detail</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4> Precision and accuracy</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4> Excellent communication skills</h4>
+              <small className="text-light">Experienced</small>
+            </article>
+            <article className="experience__details">
+              <GoVerified />
+              <h4> Ability to thrive under pressure</h4>
+              <small className="text-light">Intermediate</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
