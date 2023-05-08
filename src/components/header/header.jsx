03faf26a9061e02d6b3ce815838bed49bf3e9a60
AfrokingJasper <fortuneoliseyenum12@gmail.com>
2023-05-08 04:40:38 -0700

    mobile header socials

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,6 +3,9 @@ import "./header.css";
 import CTA from "./cta";
 import pg from "../../assest/profile-image.jpg";
 import HeaderSocials from "./headerSocials";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 
 const Header = (props) => {
   return (
@@ -13,6 +16,23 @@ const Header = (props) => {
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
         <HeaderSocials />
+        <div className="mobile__cta">
+          <a
+            href="https://www.linkedin.com/in/fortune-oliseyenum-a44960262/"
+            target="_blank"
+          >
+            <AiFillLinkedin />
+          </a>
+          <a href="https://twitter.com/TheLightHouseT5" target="_blank">
+            <AiFillTwitterSquare />
+          </a>
+          <a
+            href="https://github.com/AfrokingJasper?tab=repositories"
+            target="_blank"
+          >
+            <AiOutlineGithub />
+          </a>
+        </div>
 
         <div className="pg">
           <img src={pg} alt="fortune" />
